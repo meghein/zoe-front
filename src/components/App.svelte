2872655bfc79602b5d1c2c906995491eedcc95a8
meghein <meghan.hein@gmail.com>
2021-01-06 15:03:23 -0800

    move home page logic in to separate component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,24 +1,11 @@
 <script>
-	import { onMount } from 'svelte';
-	import { getData } from '../helpers/DataHelper'
-
-	let properties = {};
-
-	onMount(() => {
-		getData().then(res => {
-			properties = res
-		})
-	})
+	import Home from './Home.svelte'
 </script>
-
-<main>
-	{#if properties}
-	<h1>{properties.title}</h1>
-	<h2>{properties.field}</h2>
-	{@html properties.body}
-	{/if}
-</main>
 
 <style type="text/scss">
 	@import '../styles/App.scss';
 </style>
+
+<main>
+	<Home/>
+</main>
---
+++ src/components/Home.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from 'svelte';
+	import { getData } from '../helpers/DataHelper'
+
+	let properties = {};
+
+	onMount(() => {
+		getData().then(res => {
+			properties = res
+		})
+	})
+</script>
+
+<style type="text/scss">
+	@import '../styles/Home.scss';
+</style>
+
+<div>
+	{#if properties}
+	<h1>{properties.title}</h1>
+	<h2>{properties.field}</h2>
+	{@html properties.body}
+	{/if}
+</div>
