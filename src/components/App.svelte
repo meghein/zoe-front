b5762c244e42b59f27310b8375e453fac8bf98c5
meghein <meghan.hein@gmail.com>
2021-01-07 12:38:06 -0800

    add nav component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,14 +1,15 @@
 <script>
 	import { onMount } from 'svelte';
 	import { getPageData } from '../helpers/DataHelper';
+	import Nav from './Nav.svelte';
 	import Home from './Home.svelte';
 	import About from './About.svelte';
 
-	let data;
+	let pageData;
 
 	onMount(() => {
 		getPageData().then(res => {
-			data = res
+			pageData = res
 		})
 	})
 	
@@ -19,8 +20,9 @@
 </style>
 
 <main>
-	{#if data}
-	<Home content={data.home}/>
-	<About content={data.about}/>
+	{#if pageData}
+	<Nav/>
+	<Home content={pageData.home}/>
+	<About content={pageData.about}/>
 	{/if}
 </main>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,26 @@
+<script>
+
+	const list = ['About', 'Research', 'Publications', 'Contact'];
+
+	function scroll(page) {
+		document
+			.getElementById(page)
+			.scrollIntoView({ behavior: 'smooth' });
+	};
+</script>
+
+<style>
+
+</style>
+
+<nav id='nav-container'>
+	<ul id="nav-items">
+		{#each list as item }
+			<li>
+				<a href="#{item}" class="item" on:click|preventDefault={() => (scroll(item.toLowerCase()))}>
+					{item}
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
